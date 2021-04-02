8e566d21f22d5195d9d652ba81ca1a8158d1c738
Luka Jerkovic <luka.jerkovic@atos.net>
2021-04-02 16:50:02 +0200

    init

--- src/app/charts/components/chart/chart.component.ts
+++ src/app/charts/components/chart/chart.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
+import * as fc from 'd3fc';
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+  styleUrls: ['./chart.component.scss'],
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, AfterViewInit {
+  @ViewChild('chart')
+  chartContainer!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const data = fc.randomFinancial()(50);
+
+    const yExtent = fc.extentLinear().accessors([(d) => d.high, (d) => d.low]);
+
+    const xExtent = fc.extentDate().accessors([(d) => d.date]);
+
+    const gridlines = fc.annotationSvgGridline();
+    const candlestick = fc.seriesSvgCandlestick();
+    const multi = fc.seriesSvgMulti().series([gridlines, candlestick]);
+
+    const chart = fc
+      .chartCartesian(d3.scaleTime(), d3.scaleLinear())
+      .yDomain(yExtent(data))
+      .xDomain(xExtent(data))
+      .svgPlotArea(multi);
+    console.log(this, this.chartContainer);
+    d3.select(this.chartContainer.nativeElement).datum(data).call(chart);
   }
 
+  ngOnInit(): void {
+    console.log('init');
+  }
 }
---
+++ src/d3fc.d.ts
@@ -0,0 +1,15 @@
+/* declare namespace d3fc {
+  export var version: string;
+
+  namespace util {
+    export function extent(data: any, fields: any[]): any[];
+  }
+
+  namespace series {
+    export function candlestick(): any;
+  }
+} */
+
+/* declare module 'd3fc' {
+  export = d3fc;
+} */
