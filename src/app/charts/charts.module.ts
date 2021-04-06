9897d58bc8e3278ddc20e9bddb69280e9b1d0d22
Luka Jerkovic <luka.jerkovic@atos.net>
2021-04-06 16:28:19 +0200

    added plotly.js

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -1,15 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable max-len */
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PlotlyModule } from 'angular-plotly.js';
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
 import { ChartsRoutingModule } from './charts-routing.module';
 import { ChartsComponent } from './charts.component';
 import { ChartComponent } from './components/chart/chart.component';
 import { StreamingChartComponent } from './components/streaming-chart/streaming-chart.component';
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [ChartsComponent, ChartComponent, StreamingChartComponent],
-  imports: [CommonModule, ChartsRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, ChartsRoutingModule, FormsModule, ReactiveFormsModule, PlotlyModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
---
+++ src/plotly.js-dist.d.ts
@@ -0,0 +1,4 @@
+// plotly.js-dist
+declare module 'plotly.js/dist/plotly.js' {
+  export * from 'plotly.js';
+}
